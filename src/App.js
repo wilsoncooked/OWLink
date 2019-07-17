ce03a40aedf3572ddb74d6ad3726df215059d5a7
jenna <jenna_kim1@hotmail.com>
2019-07-17 18:12:44 +0200

    commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import Profile from './profile';
 
 function App() {
   return (
     <div className='App'>
       <h1>OWLink</h1>
+      <Profile />
     </div>
   );
 }
