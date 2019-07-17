f555882629a160682befca36c45191cc353e0bba
Sarah Wilsoncook <sarahlouise125@gmail.com>
2019-07-17 18:37:13 +0200

    set up basic routing

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
 import React from 'react';
 import './App.css';
+import {BrowserRouter, Route, Switch, Redirect} from 'react-router-dom';
+import Profile from './pages/Profile';
+import Timetable from './pages/Timetable';
+import NewIdeaForm from './pages/NewIdeaForm';
 
 function App() {
-  return <div className='App' />;
+  return (
+    <React.Fragment>
+      <BrowserRouter>
+        <Switch>
+          <Route path='/profile' component={Profile} />
+          <Route path='/timetable' component={Timetable} />
+          <Route path='/timetable/idea/new' component={NewIdeaForm} />
+        </Switch>
+      </BrowserRouter>
+    </React.Fragment>
+  );
 }
 
 export default App;
