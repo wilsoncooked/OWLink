c2de4ee56b6509920403fe3f4de3a93ec0427854
Sarah Wilsoncook <sarahlouise125@gmail.com>
2019-07-18 10:17:41 +0200

    firebase init

--- src/index.js
+++ src/index.js
@@ -3,6 +3,18 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import * as firebase from 'firebase';
+
+var firebaseConfig = {
+  apiKey: 'AIzaSyDSuDBGWwDlpPSu5KyHqhya5Iziyi2pFqE',
+  authDomain: 'owlink-a4f32.firebaseapp.com',
+  databaseURL: 'https://owlink-a4f32.firebaseio.com',
+  projectId: 'owlink-a4f32',
+  storageBucket: '',
+  messagingSenderId: '620782128710',
+  appId: '1:620782128710:web:25b74d543db9d394',
+};
+firebase.initializeApp(firebaseConfig);
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
