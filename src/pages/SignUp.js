06030cf22b56566f93c0dee741264ae54d07ae4c
jenna <jenna_kim1@hotmail.com>
2019-07-18 18:12:55 +0200

    firebase config

--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -3,8 +3,13 @@ import firebase from 'firebase';
 import StyledFirebaseAuth from 'react-firebaseui/StyledFirebaseAuth';
 
 firebase.initializeApp({
-  apikey: 'AIzaSyDSuDBGWwDlpPSu5KyHqhya5Iziyi2pFqE',
+  apiKey: 'AIzaSyDSuDBGWwDlpPSu5KyHqhya5Iziyi2pFqE',
   authDomain: 'owlink-a4f32.firebaseapp.com',
+  databaseURL: 'https://owlink-a4f32.firebaseio.com',
+  projectId: 'owlink-a4f32',
+  storageBucket: 'owlink-a4f32.appspot.com',
+  messagingSenderId: '620782128710',
+  appId: '1:620782128710:web:25b74d543db9d394',
 });
 
 class SignUp extends Component {
