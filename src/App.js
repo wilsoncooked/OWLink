6e26e46d77dd392062617cb266fbc93203bec497
jenna <jenna_kim1@hotmail.com>
2019-07-18 15:29:57 +0200

    created Notification page and added route in App

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import {BrowserRouter, Route, Switch, Redirect} from 'react-router-dom';
 import Home from './pages/Home';
 import Profile from './pages/Profile';
+import Notification from './pages/Notification';
 import TimetableOverview from './pages/TimetableOverview';
 import NewIdeaForm from './pages/NewIdeaForm';
 
@@ -13,6 +14,7 @@ function App() {
         <Switch>
           <Route exact path='/' component={Home} />
           <Route path='/profile' component={Profile} />
+          <Route path='notification' component={Notification} />
           <Route path='/timetable' component={TimetableOverview} />
           <Route path='/timetable/idea/new' component={NewIdeaForm} />
         </Switch>
