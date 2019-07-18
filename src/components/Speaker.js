17b107f6adee2d315b0f22b8d3aa8e88d19354d8
Campalo <25295489+Campalo@users.noreply.github.com>
2019-07-18 12:45:05 +0200

    create speaker component WIP

--- src/components/Speaker.js
+++ src/components/Speaker.js
@@ -5,6 +5,8 @@ import CardActionArea from '@material-ui/core/CardActionArea';
 import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
 import Typography from '@material-ui/core/Typography';
+// Link
+import {Link} from '@react-router-dom';
 
 function Speaker() {
   return (
